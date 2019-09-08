9de9083baf89c5e9dd22d3b78fd164d655992aa6
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-09-08 05:44:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,8 @@
 def square_array(array)
-  temp = []
   counter = 0
   while counter < array.length do
     array[counter] = array[counter] ** 2
-    temp.push(array[counter])
     counter += 1
   end
-  temp
+  array
 end
